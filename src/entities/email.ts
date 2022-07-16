4ab4670ac9f6513672d7d5a44fd579f3b4d176f2
felpgoes <felipe.goess@hotmail.com>
2022-07-16 19:07:35 -0300

    feat: validate local part larger then 64 chars

--- src/entities/email.ts
+++ src/entities/email.ts
@@ -1,6 +1,11 @@
 export class Email {
   static validate (email: string): boolean {
     if (!email) return false
+
+    const [local] = email.split('@')
+
+    if (local.length > 64) return false
+
     return true
   }
 }
