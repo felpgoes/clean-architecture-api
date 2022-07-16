44196a3b34e19cf6f6e79c38aa026466272557bb
felpgoes <felipe.goess@hotmail.com>
2022-07-16 20:17:54 -0300

    feat: create valid user

---
+++ src/entities/user.ts
@@ -0,0 +1,29 @@
+import { Either, left, right } from '../shared/Either'
+import { Email } from './email'
+import { InvalidEmailError } from './errors/invalid-email-error'
+import { InvalidNameError } from './errors/invalid-name-error'
+import { Name } from './name'
+import { UserData } from './user-data'
+
+export class User {
+  public readonly email: Email
+  public readonly name: Name
+
+  private constructor (name: Name, email: Email) {
+    this.name = name
+    this.email = email
+  }
+
+  static create (userData: UserData): Either<InvalidEmailError | InvalidNameError, User> {
+    const nameOrError = Name.create(userData.name)
+    if (nameOrError.isLeft()) return left(new InvalidNameError())
+
+    const emailOrError = Email.create(userData.email)
+    if (emailOrError.isLeft()) return left(new InvalidEmailError())
+
+    const name = nameOrError.value as Name
+    const email = emailOrError.value as Email
+
+    return right(new User(name, email))
+  }
+}
