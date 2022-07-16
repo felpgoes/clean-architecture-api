95612f92157081c53b2a9ceb3bd812fa6d49f017
felpgoes <felipe.goess@hotmail.com>
2022-07-16 20:17:59 -0300

    test: create valid user

--- src/entities/user.spec.ts
+++ src/entities/user.spec.ts
@@ -1,7 +1,7 @@
 import { left } from '../shared/Either'
 import { InvalidEmailError } from './errors/invalid-email-error'
 import { InvalidNameError } from './errors/invalid-name-error'
-import { User } from './User'
+import { User } from './user'
 
 describe('User domain entity', () => {
   test('should not create user with invalid email address', () => {
@@ -20,5 +20,14 @@ describe('User domain entity', () => {
     const invalidName = 'O'.repeat(256)
     const error = User.create({ name: invalidName, email: 'any@email.com' })
     expect(error).toEqual(left(new InvalidNameError()))
+  })
+
+  test('should create user with valid data', () => {
+    const validName = 'Any Name'
+    const validEmail = 'any@mail.com'
+
+    const user = User.create({ name: validName, email: validEmail }).value as User
+    expect(user.email.value).toEqual(validEmail)
+    expect(user.name.value).toEqual(validName)
   })
 })
